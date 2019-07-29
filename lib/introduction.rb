c779fa6a9d74c366fbed07819b632edf62dde2f2
Chaya Deaver <chayadeaver@gmail.com>
2019-07-29 17:40:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,16 +1,16 @@
 name = "Josh"
 name = "Dan"
-language = "Ember"
+language = "Ember.js"
 language = "React"
 
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction(Josh)
+introduction("Josh")
 
 def introduction_with_language(name, language)
   puts "Hello, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Dan, Ember)
+introduction_with_language("Dan", "Ember.js")
