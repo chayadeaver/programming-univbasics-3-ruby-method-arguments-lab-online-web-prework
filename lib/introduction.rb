33316a98f828427ed8da8e7580cd6134a2b43866
Chaya Deaver <chayadeaver@gmail.com>
2019-07-29 17:38:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,4 +13,4 @@ def introduction_with_language(name, language)
   puts "Hello, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Dan, language)
+introduction_with_language(Dan, Ember)
