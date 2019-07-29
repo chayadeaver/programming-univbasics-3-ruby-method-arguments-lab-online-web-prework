188c7a478153d9849dbe1e260e900c78303e55bc
Chaya Deaver <chayadeaver@gmail.com>
2019-07-29 17:34:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,13 +1,14 @@
 name = "Chaya"
-language = "Ruby"
+
 
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
 introduction(name)
-
-def introduction_with_language(name, language)
+name = "Micah"
+language = "Ruby"
+def introduction_with_language(Micah, language)
   puts "Hello, my name is #{name} and I am learning to program in #{language}."
 end
 
