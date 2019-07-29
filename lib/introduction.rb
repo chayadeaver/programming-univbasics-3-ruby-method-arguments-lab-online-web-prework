19c8f8f2604156b06c3fbc5553cd36f4e40051e4
Chaya Deaver <chayadeaver@gmail.com>
2019-07-29 18:57:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -25,6 +25,6 @@ def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language} "
 end
 
-introduction_with_language_optional("Edwin", 'default')
+introduction_with_language_optional("Edwin")
 introduction_with_language_optional("Edwin", "Python")
 
